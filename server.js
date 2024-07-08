38462643fe73e6e0f26e14a13641ec7dabf7160b
GunHyuk Lee <37396444+Sy2tema@users.noreply.github.com>
2024-07-08 14:15:58 +0900

    feat: Socket.io 서버 기본 구조 세팅

--- server.js
+++ server.js
@@ -8,9 +8,17 @@ const server = http.createServer(app);
 
 const io = new Server(server);
 
-io.on("connection", () => { /*... */ });
+io.on("connection", (client) => {
+    console.log(client, "사용자가 로그인했습니다.");
+});
 server.listen(3000, () => {
     console.log("서버에서 듣고있습니다...:3000");
+});
+
+app.get("/message", (_, res) => res.send("Hello from express!"));
+
+app.get("/api", (_, res) => {
+    res.send("Hello from api!");
 });
 
 ViteExpress.bind(app, server);
